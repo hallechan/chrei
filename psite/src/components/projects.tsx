b1d97b444362798b5ff838aeb6594aee875ff373
hailey <hhaileychhan@gmail.com>
2024-01-10 20:30:07 -0500

    add cloak in projects
    i am working on a new ux prototype!

--- psite/src/components/projects.tsx
+++ psite/src/components/projects.tsx
@@ -40,6 +40,37 @@ const Projects = () => {
               </a>
             </div>
           </div>
+          <div className="flex">
+            <img
+              className="h-[512px] animate-fade-right animate-delay-500"
+              src="/assets/images/cloak.png"
+            />
+            <div className="m-12 w-min">
+              <h1
+                className="h-12 animate-typing animate-once
+              overflow-hidden whitespace-nowrap border-r-2 border-r-[var(--offwhite)] 
+            text-[var(--offwhite)] text-6xl font-bold uppercase"
+              >
+                cloak
+              </h1>
+              <p className="mt-8 text-[var(--offwhite)] text-2xl font-reg animate-fade-left animate-delay-1000">
+                a virtual closet <br />
+                <br /> I created a UI/UX design prototype to virtually organize
+                clothes on your wishlist. <br />
+              </p>
+              <a>
+                <button
+                  className="my-8 border-solid border-2 border-[var(--offwhite)] rounded-full
+        hover:animate-jump hover:bg-[var(--pink)] hover:border-[var(--pink)] hover:animate-ease-in-out
+        animate-fade-left animate-delay-1000 animate-once"
+                >
+                  <h1 className="m-4 ml-8 mr-8 text-[var(--offwhite)] text-center text-2xl font-reg">
+                    coming...
+                  </h1>
+                </button>
+              </a>
+            </div>
+          </div>
         </li>
       </ul>
       <div className="mt-16 mb-4 flex justify-center">
