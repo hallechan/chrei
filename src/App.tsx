f36bfba30ac738233f3570926f6b4420797c80e1
hailey <hhaileychhan@gmail.com>
2024-01-30 21:34:01 -0500

    changed it to /react
    please work this time speed insights

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Analytics } from "@vercel/analytics/react";
-import { SpeedInsights } from "@vercel/speed-insights/next";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 
 {
   /* MAIN WEBSITE */
